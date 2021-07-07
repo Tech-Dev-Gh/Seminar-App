817cec002d1c855ae138e46188756a9c0f8cc048
BRIM-TECH-TEAM <86945391+BRIM-TECH-TEAM@users.noreply.github.com>
2021-07-07 06:17:26 +0000

    Updates
    I added the constant.dart file to hold all constant contents to be used throughout the app. I also added the vehicle model and created an array/List of all vehicles for the app. I updated the pubspec.yaml file by including the assets and fonts

---
+++ lib/constants.dart
@@ -0,0 +1,7 @@
+import "package:flutter/material.dart";
+
+final kprimaryColor = const Color(0xFA7F35);
+final ksecondaryColor = const Color(0xFDEBC9);
+
+const kbalooFont = "Baloo";
+const kPoppinsfont = "Poppins";
